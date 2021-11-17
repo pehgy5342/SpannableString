56797e54150f4267bad9a81e569c664c3945368f
Peggy <pehgy5342z@gmail.com>
2021-11-17 17:45:14 +0800

    SpannableString

--- app/src/main/java/com/example/spannablestring/MainActivity.kt
+++ app/src/main/java/com/example/spannablestring/MainActivity.kt
@@ -1,32 +1,35 @@
 package com.example.spannablestring
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.BackgroundColorSpan
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val lyrics1 = "太多人愛抱怨但hustle程度接近zero"
+    val lyrics2 = "當個厭世少年沒用 只會更加emo"
+    val lyrics3 = "沒啥成就不是因為個性平凡"
+    val lyrics4 = "是你踏不出舒適圈 打破惡性循環"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-
-//        val lyrics2 = "當個厭世少年沒用 只會更加emo"
-//        val lyrics3 = "沒啥成就不是因為個性平凡"
-//        val lyrics4 = "是你踏不出舒適圈 打破惡性循環"
-
         changeTextAppearance()
 
     }
 
-    private fun changeTextAppearance(){
-        val lyrics1 = "太多人愛抱怨但hustle程度接近zero"
-        val spannable = SpannableStringBuilder(lyrics1)
-
-
-
-
-
+    private fun changeTextAppearance() {
+        val text = findViewById<TextView>(R.id.text)
+        val spannable = SpannableStringBuilder()
+        spannable.append(lyrics1)
+        spannable.setSpan(BackgroundColorSpan(Color.YELLOW), 7, 13, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        text.text = spannable
     }
 
 }
